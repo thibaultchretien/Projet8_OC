d2b44fa1ada4499b3f46931ef16ff9a0b8a9628c
thibaultchretien <thibault.chretien021@icloud.com>
2024-11-08 19:41:46 +0800

    Update api.py

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 from PIL import Image
 import numpy as np
+import io
 
 # Charger le modèle
 try:
@@ -16,10 +17,10 @@ except Exception as e:
 app = Flask(__name__)
 
 # Fonction pour prédire le mask à partir de l'image
-def predict_mask(image_path):
+def predict_mask(image_bytes):
     try:
-        # Charger l'image
-        image = Image.open(image_path)
+        # Convertir les bytes en image
+        image = Image.open(io.BytesIO(image_bytes))
         image = image.resize((256, 256))  # Redimensionner l'image
         image = np.array(image)
         image = np.expand_dims(image, axis=0)  # Ajouter la dimension batch
@@ -43,24 +44,24 @@ def predict():
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
 
-    # Sauvegarder l'image reçue dans le répertoire courant
-    file_path = file.filename  # Sauvegarder directement avec le nom du fichier dans le répertoire courant
     try:
-        file.save(file_path)
+        # Lire l'image directement en mémoire
+        image_bytes = file.read()
+
+        # Prédire le mask
+        mask, error = predict_mask(image_bytes)
+        if mask is None:
+            return jsonify({'error': f"Erreur lors de la prédiction: {error}"}), 500
+
+        # Sauvegarder le masque prédit dans un format de retour pour l'affichage (sans fichier)
+        mask_image = Image.fromarray(mask.astype(np.uint8))
+        mask_image_path = 'predicted_mask.png'  # Masque retourné dans un format d'image
+        mask_image.save(mask_image_path)
+
+        return jsonify({'message': 'Prediction complete', 'mask_image': mask_image_path})
+
     except Exception as e:
-        return jsonify({'error': f"Erreur lors de la sauvegarde de l'image: {str(e)}"}), 500
-
-    # Prédire le mask
-    mask, error = predict_mask(file_path)
-    if mask is None:
-        return jsonify({'error': f"Erreur lors de la prédiction: {error}"}), 500
-
-    # Sauvegarder le mask prédit dans le répertoire courant
-    mask_image = Image.fromarray(mask.astype(np.uint8))
-    mask_image_path = 'predicted_mask.png'  # Sauvegarder le masque avec un nom fixe dans le répertoire courant
-    mask_image.save(mask_image_path)
-
-    return jsonify({'message': 'Prediction complete', 'mask_image': mask_image_path})
+        return jsonify({'error': f"Erreur: {str(e)}"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
