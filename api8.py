2f513507beb98fa3c83c29b12f9f286c57b4910a
thibaultchretien <thibault.chretien021@icloud.com>
2024-11-08 11:44:47 +0800

    Update api8.py

--- api8.py
+++ api8.py
@@ -2,15 +2,19 @@ import os
 import numpy as np
 import tensorflow as tf
 from flask import Flask, request, jsonify
-from tensorflow.keras.models import load_model
 from PIL import Image
 import io
 
 # Initialisation de l'application Flask
 app = Flask(__name__)
 
-# Charger le modèle de segmentation
-model = load_model('model_unet.h5')
+# Charger le modèle TensorFlow Lite
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+# Obtenir les détails des tensors d'entrée et de sortie
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 # Route home pour vérifier que l'API fonctionne
 @app.route('/')
@@ -20,9 +24,9 @@ def home():
 # Fonction pour prétraiter l'image avant de l'envoyer au modèle
 def preprocess_image(image):
     # Redimensionner l'image à la taille d'entrée du modèle
-    image = image.resize((256, 256))  # Ajuste la taille en fonction de ton modèle
+    image = image.resize((256, 256))  # Ajustez la taille si nécessaire
     image = np.array(image) / 255.0  # Normalisation
-    image = np.expand_dims(image, axis=0)  # Ajouter la dimension du batch
+    image = np.expand_dims(image, axis=0).astype(np.float32)  # Ajouter la dimension du batch
     return image
 
 # Route pour effectuer la prédiction
@@ -39,11 +43,13 @@ def predict():
     image = Image.open(io.BytesIO(file.read()))
     preprocessed_image = preprocess_image(image)
     
-    # Faire une prédiction avec le modèle
-    prediction = model.predict(preprocessed_image)
+    # Faire une prédiction avec le modèle TensorFlow Lite
+    interpreter.set_tensor(input_details[0]['index'], preprocessed_image)
+    interpreter.invoke()
+    prediction = interpreter.get_tensor(output_details[0]['index'])
     
     # Convertir le masque en image et le renvoyer
-    mask = prediction[0]  # Si ton modèle renvoie un batch, prendre le premier élément
+    mask = prediction[0]  # Si le modèle renvoie un batch, prendre le premier élément
     mask = (mask > 0.5).astype(np.uint8) * 255  # Binariser le masque si nécessaire
     mask_image = Image.fromarray(mask)
 
@@ -55,4 +61,3 @@ def predict():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
